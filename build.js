fc6b3b4588996e265a2fcd8e7dbf9d3fdf321e64
ulrich-barnstedt <ulrich.barnstedt@gmail.com>
2025-11-10 00:38:09 +0100

    optimize esbuild config

--- build.js
+++ build.js
@@ -3,5 +3,8 @@ import * as esbuild from 'esbuild';
 await esbuild.build({
     bundle: true,
     entryPoints: ['src/index.ts'],
-    outfile: 'public/dist/bundle.js'
+    outfile: 'public/dist/bundle.js',
+    treeShaking: true,
+    sourcemap: false,
+    minify: true
 });
